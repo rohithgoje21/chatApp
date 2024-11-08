332ed84df423f9c1a32557eef784761033385153
rohithgoje21 <rohithgoje21@gmail.com>
2024-11-09 00:15:50 +0530

    Initial-Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 8080;
+
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
