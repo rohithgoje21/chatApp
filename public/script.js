43d3dcafbfb12427d24164fa24dabfd88de59668
rohithgoje21 <rohithgoje21@gmail.com>
2024-11-11 00:28:32 +0530

    Dymanically displaying messages that the user typed

--- public/script.js
+++ public/script.js
@@ -9,26 +9,85 @@ document
       .querySelector(".join-screen #username-input")
       .value.trim();
     if (username !== "") {
-      socket.emit("newuser", username);
       uname = username;
+      socket.emit("newuser", uname);
+      displaymessage("update", {
+        username: uname,
+        text: " joined the conversation",
+      });
       document.querySelector(".join-screen").classList.remove("active");
       document.querySelector(".chat-screen").classList.add("active");
-      document.querySelector(".join-screen #username-input").value = "";
     }
+    document.querySelector(".join-screen #username-input").value = "";
   });
+
+document
+  .querySelector(".chat-screen #send-message")
+  .addEventListener("click", function () {
+    let message = document
+      .querySelector(".chat-screen #message-input")
+      .value.trim();
+    if (message !== "") {
+      displaymessage("my", { username: uname, text: message });
+      socket.emit("chat", {
+        username: uname,
+        text: message,
+      });
+    }
+    document.querySelector(".chat-screen #message-input").value = "";
+  });
+
 document
   .querySelector(".chat-screen #exit-chat")
   .addEventListener("click", function () {
     socket.emit("exituser", uname);
+    displaymessage("update", {
+      username: uname,
+      text: " left the conversation",
+    });
     document.querySelector(".chat-screen").classList.remove("active");
     document.querySelector(".join-screen").classList.add("active");
   });
 
+function displaymessage(type, message) {
+  if (type == "my") {
+    let mymessage = document.createElement("div");
+    mymessage.classList.add("my-message", "message");
 
-// socket.on("newuser", function (username) {
-//   document.querySelector(".chat-screen.update").innerHTML += `<li>${username}</li>`;
-// });
+    let name = document.createElement("div");
+    name.classList.add("name");
+    name.textContent = "You";
 
-// socket.on("exituser", function (username) {
-//   document.querySelector(`.chat-screen.update li:contains(${username})`).remove();
-// });
+    let text = document.createElement("div");
+    text.classList.add("text");
+    text.textContent = message.text;
+
+    let div = document.createElement("div");
+    div.append(name, text);
+
+    mymessage.appendChild(div);
+    document.querySelector(".chat-screen .messages").appendChild(mymessage);
+  } else if (type == "other") {
+    let othermessage = document.createElement("div");
+    othermessage.classList.add("other-message", "message");
+
+    let name = document.createElement("div");
+    name.classList.add("name");
+    name.textContent = message.username;
+
+    let text = document.createElement("div");
+    text.classList.add("text");
+    text.textContent = message.text;
+
+    let div = document.createElement("div");
+    div.append(name, text);
+
+    othermessage.appendChile(div);
+    document.querySelector(".chat-screen .messages").appendChild(othermessage);
+  } else {
+    let update = document.createElement("div");
+    update.classList.add("update");
+    update.textContent = message.username + message.text;
+    document.querySelector(".chat-screen .messages").appendChild(update);
+  }
+}
